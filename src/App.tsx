79744a429e9ef282aa63065cd40174b5e230d49f
TarasLozinskyi <tlozinskyi@gmail.com>
2024-05-08 10:40:50 +0300

    homework3 task2

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {usePrevious} from "./customHooks/usePrevious";
+
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    const [current, setCurrent] = useState<number>(0)
+    const ref = usePrevious(current);
+    const change = () => {
+        setCurrent(prevState => prevState += 1);
+    }
+    return (
+        <div className="App">
+            <h2>useRef: {ref}</h2>
+            <h2>useState: {current}</h2>
+            <button onClick={change}>click</button>
+        </div>
+    );
 }
 
 export default App;
